d2620e26aebf3bb7e102ed8feed7f70a7df2f106
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-11-28 00:18:14 -0800

    finished button
    Added array of dice images and added image change in button function

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
+    let imageNameArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -26,7 +28,8 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
-        
+        diceImageView1.image = UIImage(named: imageNameArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: imageNameArray[randomDiceIndex2])
     }
     
 }
