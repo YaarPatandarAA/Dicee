4e9138d6b54b6a0c35bc0eca844fa8dc86fb0228
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-11-28 00:36:02 -0800

    added counter
    added counter to amount of button presses

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,19 +12,29 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
+    var counter = 0
     
     let imageNameArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var timesButtonPressed: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDice()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //What Happends when the button is pressed
+        updateDice()
+        counter+=1
+        timesButtonPressed.text = "Times Pressed: \(counter)"
+        
+    }
+    
+    func updateDice(){
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
